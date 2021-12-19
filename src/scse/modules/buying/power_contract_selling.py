5a70e2e79ce7c36d410f22d8167e3aae5126f04d
Conor Perreault <cperreault@wisc.edu>
2021-12-19 17:12:40 -0600

    test

---
+++ src/scse/modules/buying/power_contract_selling.py
@@ -0,0 +1,64 @@
+from scse.api.module import Agent
+import numpy as np
+import scipy.stats
+import logging
+
+logger = logging.getLogger(__name__)
+DEFAULT_GUARANTEED_PRICE = 10
+DEFAULT_BACKUP_PRICE = 5
+class SimpleFlexibleFirm(Agent):
+    def __init__(self, run_parameters):
+        simulation_seed = run_parameters['simulation_seed']
+        self._rng = np.random.RandomState(simulation_seed)
+        self.capacity = run_parameters['firm_capacity']
+        
+    def get_name(self):
+        return 'selling_contracts'
+    
+    def reset(self, context, state):
+        self._asin_list = context['asin_list']
+        # again, not sure what belongs in here
+    
+    def compute_actions(self, state):
+        guaranteed_power = .5 # ratios
+        backup_power = .5
+        not_sold = 0
+        current_clock = state['clock']
+        # might need to do one asin for each type of purchase, although
+        # that gets a little bit weird. Look further into the 
+        # 'settling' functions and see what othe types of 
+        # things I can add into the state.
+
+        # For now: fixed price. remaining supply is just a random 
+        # draw based on renewables, what's available, etc.
+        # that should be easy enough to test and modify in the future
+
+        # If I can get that working, set up a meeting with the others
+        # and discuss what type of experiments we actually want to setup
+        # and it should be easy to make the code modifications at that
+        # point 
+
+        # In this simplest model, it's assumed that someone else can cover
+        # up the backup supply as needed. It's not really important, it's 
+        # just how to get something up and running quickly
+
+        # the structure of these actions will depend on how I can make the 
+        # profit calculations, so I'll need to look at that.
+        action1 = {
+            'type': 'bid',
+            'bid_type': 'guaranteed',
+            'price': DEFAULT_GUARANTEED_PRICE,
+            'quantity': guaranteed_power * self.capacity,
+            'schedule': current_clock,
+            'bidder': 1 # 1 is bidder of interest
+        }
+        action2 = {
+            'type': 'bid',
+            'bid_type': 'backup',
+            'price': DEFAULT_BACKUP_PRICE,
+            'quantity': backup_power * self.capacity,
+            'schedule': current_clock,
+            'bidder': 1
+        }
+        actions = [action1, action2]
+        return actions
---
+++ src/scse/modules/buying/power_rest_of_market.py
@@ -0,0 +1,41 @@
+from scse.api.module import Agent
+import numpy as np
+import scipy.stats
+import logging
+
+logger = logging.getLogger(__name__)
+DEFAULT_GUARANTEED_PRICE = 10
+DEFAULT_BACKUP_PRICE = 5
+DEFAULT_MEAN = 25
+DEFAULT_VAR = 2
+class SimpleRestOfMarket(Agent):
+    def __init__(self, run_parameters):
+        simulation_seed = run_parameters['simulation_seed']
+        self._rng = np.random.RandomState(simulation_seed)
+        
+    def get_name(self):
+        return 'market_selling_contracts'
+
+    def reset(self, context, state):
+        self._asin_list = context['asin_list']
+        # again, not sure
+
+    def compute_actions(self, state):
+        # compute the "guaranteed supply"-- whatever other firms bid
+        # as guaranteed power + whatever renewables come in
+
+        # note that at some point, state can hold information about
+        # projected demand/supply that everyone has access to. Then,
+        # the question is how strategies change under increasing uncertainty (renewable)
+        quantity = self._rng.normal(DEFAULT_MEAN,DEFAULT_VAR)
+        current_clock = state['clock']
+
+        action = {
+            'type': 'bid',
+            'bid_type': 'guaranteed',
+            'price': DEFAULT_GUARANTEED_PRICE,
+            'quantity': quantity,
+            'schedule':current_clock,
+            'bidder': 0 # 0 is market
+        }
+        return [action]
