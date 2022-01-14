c4248b0252730ef89caee068ccfa11d49349abb9
Pedro Moll <pedrobernar10@gmail.com>
2022-01-14 01:54:36 -0300

    Rename firms and models

--- src/scse/modules/production/non_renewables_firm.py
+++ src/scse/modules/production/non_renewables_firm.py
@@ -3,51 +3,51 @@ import numpy as np
 import scipy.stats
 import logging
 from scse.modules.production.supply_model import SupplyModel
+from scse.modules.buying.power_rest_of_market import DEFAULT_BACKUP_PRICE
 
 logger = logging.getLogger(__name__)
 
 
-class FlexibleFirm(Agent):
+class NonRenewablesFirm(Agent):
     """
-    Flexible firm, like a natural gas company
+    Produces power from a non-renewables source, such as coal or natural gas.
+    More predictable output, higher cost per MW.
     """
-    DEFAULT_RAMP_UP_COST = 20
-    DEFAULT_COST_PER_UNIT = 6
+
+    DEFAULT_RAMP_UP_COST = 40
+    DEFAULT_COST_PER_UNIT = 3
 
     def __init__(self, run_parameters):
         simulation_seed = run_parameters['simulation_seed']
         self._rng = np.random.RandomState(simulation_seed)
-        self.capacity = run_parameters['flexible_capacity']
-        self.default_price = run_parameters['flexible_price']
+        self.capacity = run_parameters['static_capacity']
+        self.default_price = run_parameters['static_price']
         self.supply_model = SupplyModel(
-            model_name='model_r',
-            mean=0.400,
-            var=0.050,
-            offset=75041.9,
-            scale=196648.1
+            model_name='supply-model-non-ren',
+            mean=0.418,
+            var=0.056,
+            offset=1557416.5,
+            scale=1357952.1
         )
 
     def get_name(self):
-        return 'flexible_firm'
+        return 'static_firm'
 
     def reset(self, context, state):
         return None
-        # again, not sure what belongs in here
 
     def compute_actions(self, state):
         current_clock = state['clock']
         capacity = self.supply_model.predict(state['date_time'])
-        # if we want to incorporate strategy to these bids, 'quantity' can
-        # be set to the maximum that the firm is willing to sell at either
-        # guaranteed or backup power, set the other price to inf
         action = {
             'type': 'bid',
             'price': self.default_price,
             'quantity': capacity,
             'schedule': current_clock,
-            'bidder': 'flexible',
-            # 'rampup_cost': self.DEFAULT_RAMP_UP_COST,
-            # 'cost_pu': self.DEFAULT_COST_PER_UNIT
+            'bidder': 'static',
+            # 'cost_pu': self.DEFAULT_COST_PER_UNIT,
+            # 'ramup_cost': self.DEFAULT_RAMP_UP_COST,
         }
+        #logger.debug("static firm produced {} units of power".format(capacity))
         actions = [action]
         return actions
---
+++ src/scse/modules/production/renewables_firm.py
@@ -0,0 +1,54 @@
+from scse.api.module import Agent
+import numpy as np
+import scipy.stats
+import logging
+from scse.modules.production.supply_model import SupplyModel
+
+logger = logging.getLogger(__name__)
+
+
+class RenewablesFirm(Agent):
+    """
+    Produces energy from renewables sources, such as wind or solar.
+    Less predictable output, higher price per MW.
+    """
+    DEFAULT_RAMP_UP_COST = 20
+    DEFAULT_COST_PER_UNIT = 6
+
+    def __init__(self, run_parameters):
+        simulation_seed = run_parameters['simulation_seed']
+        self._rng = np.random.RandomState(simulation_seed)
+        self.capacity = run_parameters['flexible_capacity']
+        self.default_price = run_parameters['flexible_price']
+        self.supply_model = SupplyModel(
+            model_name='supply-model-ren',
+            mean=0.400,
+            var=0.050,
+            offset=75041.9,
+            scale=196648.1
+        )
+
+    def get_name(self):
+        return 'flexible_firm'
+
+    def reset(self, context, state):
+        return None
+        # again, not sure what belongs in here
+
+    def compute_actions(self, state):
+        current_clock = state['clock']
+        capacity = self.supply_model.predict(state['date_time'])
+        # if we want to incorporate strategy to these bids, 'quantity' can
+        # be set to the maximum that the firm is willing to sell at either
+        # guaranteed or backup power, set the other price to inf
+        action = {
+            'type': 'bid',
+            'price': self.default_price,
+            'quantity': capacity,
+            'schedule': current_clock,
+            'bidder': 'flexible',
+            # 'rampup_cost': self.DEFAULT_RAMP_UP_COST,
+            # 'cost_pu': self.DEFAULT_COST_PER_UNIT
+        }
+        actions = [action]
+        return actions
