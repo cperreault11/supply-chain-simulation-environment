2ed7c0a55498211e08f4bc6f94df3cc1d3e5d1c8
Pedro Moll <pedrobernar10@gmail.com>
2022-01-18 08:45:36 -0300

    Add days in year logic

--- src/scse/modules/production/supply_model.py
+++ src/scse/modules/production/supply_model.py
@@ -1,6 +1,7 @@
 import GPy
 import pickle
 import numpy as np
+import calendar
 
 
 class SupplyModel():
@@ -19,11 +20,13 @@ class SupplyModel():
         self.last_prediction = np.random.normal(self.mean, self.var)
 
     def predict(self, date):
+        days_in_year = 366 if calendar.isleap(date.year) else 365
         input = np.array(
-            [[date.timetuple().tm_yday, date.weekday()/6.0, self.last_prediction]])
+            [[date.timetuple().tm_yday / days_in_year, date.weekday()/6.0, self.last_prediction]])
         self.last_prediction = \
             self.model.posterior_samples(input, size=1)[0][0][0]
-        while self.last_prediction * self.scale + self.offset < 0: # resample to avoid negative values. shouldn't occur too often
+        # resample to avoid negative values. shouldn't occur too often
+        while self.last_prediction * self.scale + self.offset < 0:
             self.last_prediction = \
                 self.model.posterior_samples(input, size=1)[0][0][0]
         return self.last_prediction * self.scale + self.offset, np.sqrt(self.model.predict(input)[1])[0][0] * self.scale
